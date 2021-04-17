1888cf925ba8d298c68f2030c397b5e64e34f28b
hainvd00641fpt <hainvd00641@fpt.edu.vn>
2021-04-17 17:19:14 +0700

    React hooks: useState() through a simple example

---
+++ react-hooks-basic/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import ColorBox from './component/ColorBox';
+
+function App() {
+  return (
+    <div className="app">
+      <ColorBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-hooks-basic/src/component/ColorBox/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import './ColorBox.scss'
+import PropTypes from 'prop-types'
+
+function ColorBox() {
+    
+    const [color, setColor] = useState( () => {
+        const initColor = localStorage.getItem('box-color') || 'pink';
+        return initColor;
+    });
+
+    function getRandomColor(){
+        const listColor = ['green', 'blue', 'red', 'orange', 'yellow'];
+        const randomIndex = Math.trunc(Math.random()*5);
+        return listColor[randomIndex];
+    }
+
+    function handleBoxColor() {
+        const newColor = getRandomColor();
+        setColor(newColor);
+
+        localStorage.setItem('box-color', newColor);
+    }
+
+    return (
+        <div className="color-box" 
+        style={{backgroundColor: color}}
+        onClick={handleBoxColor}
+        >
+            Color box
+        </div>
+    )
+}
+
+ColorBox.propTypes = {
+
+}
+
+export default ColorBox
+
